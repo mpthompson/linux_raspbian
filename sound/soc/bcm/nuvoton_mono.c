a69bb2bd8dc4a4322f388d4bd0003e4c3df93292
Mike Thompson <mpthompson@gmail.com>
2016-01-30 00:54:21 -0800

    Added the initialization of the Nuvoton Mono DAC.
    Code is still untested, but at least it compiles.

--- sound/soc/bcm/nuvoton_mono.c
+++ sound/soc/bcm/nuvoton_mono.c
@@ -25,21 +25,150 @@
 
 #include "../codecs/wm8510.h"
 
+static const struct snd_soc_dapm_widget nuvoton_mono_dapm_widgets[] = {
+	SND_SOC_DAPM_MIC("Int Mic", NULL),
+	SND_SOC_DAPM_SPK("Ext Spk", NULL),
+};
+
+static const struct snd_soc_dapm_route nuvoton_mono_audio_map[] = {
+	/* speaker connected to SPKOUT */
+	{"Ext Spk", NULL, "SPKOUTP"},
+	{"Ext Spk", NULL, "SPKOUTN"},
+	{"Mic Bias", NULL, "Int Mic"},
+	{"MICN", NULL, "Mic Bias"},
+	{"MICP", NULL, "Mic Bias"},
+};
+
 static int snd_rpi_nuvoton_mono_init(struct snd_soc_pcm_runtime *rtd)
 {
+	struct snd_soc_dapm_context *dapm = &rtd->card->dapm;
+	struct snd_soc_dai *codec_dai = rtd->codec_dai;
+
+	/* Add DAPM widgets */
+	snd_soc_dapm_new_controls(dapm, nuvoton_mono_dapm_widgets, 
+					ARRAY_SIZE(nuvoton_mono_dapm_widgets));
+
+	/* Setup audio path interconnects */
+	snd_soc_dapm_add_routes(dapm, nuvoton_mono_audio_map, 
+					ARRAY_SIZE(nuvoton_mono_audio_map));
+
+	/* Always connected pins */
+	snd_soc_dapm_enable_pin(dapm, "Int Mic");
+	snd_soc_dapm_enable_pin(dapm, "Ext Spk");
+	snd_soc_dapm_sync(dapm);
+
+	/* Make CSB show PLL rate */
+	snd_soc_dai_set_clkdiv(codec_dai, WM8510_OPCLKDIV,
+					WM8510_OPCLKDIV_1 | 4);
+
 	return 0;
 }
 
 static int snd_rpi_nuvoton_mono_hw_params(struct snd_pcm_substream *substream,
-				       struct snd_pcm_hw_params *params)
+					struct snd_pcm_hw_params *params)
 {
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
+	struct snd_soc_dai *codec_dai = rtd->codec_dai;
 	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
+	unsigned int pll_out = 0, bclk = 0, mclk_div = 0;
+	int ret;
 
-	unsigned int sample_bits =
-		snd_pcm_format_physical_width(params_format(params));
+	/* Because the Raspberry Pi doesn't produce MCLK, we're going to 
+	 * let the CODEC be in charge of all the clocks 
+	 */
+	const unsigned int fmt = (SND_SOC_DAIFMT_I2S |
+				  SND_SOC_DAIFMT_NB_NF |
+				  SND_SOC_DAIFMT_CBM_CFM);
 
-	return snd_soc_dai_set_bclk_ratio(cpu_dai, sample_bits * 2);
+	/* Figure out PLL and BCLK dividers for WM8510 */
+	switch (params_rate(params)) {
+	case 48000:
+		pll_out = 24576000;
+		mclk_div = WM8510_MCLKDIV_2;
+		bclk = WM8510_BCLKDIV_8;
+		break;
+
+	case 44100:
+		pll_out = 22579200;
+		mclk_div = WM8510_MCLKDIV_2;
+		bclk = WM8510_BCLKDIV_8;
+		break;
+
+	case 22050:
+		pll_out = 22579200;
+		mclk_div = WM8510_MCLKDIV_4;
+		bclk = WM8510_BCLKDIV_8;
+		break;
+
+	case 16000:
+		pll_out = 24576000;
+		mclk_div = WM8510_MCLKDIV_6;
+		bclk = WM8510_BCLKDIV_8;
+		break;
+
+	case 11025:
+		pll_out = 22579200;
+		mclk_div = WM8510_MCLKDIV_8;
+		bclk = WM8510_BCLKDIV_8;
+		break;
+
+	case 8000:
+		pll_out = 24576000;
+		mclk_div = WM8510_MCLKDIV_12;
+		bclk = WM8510_BCLKDIV_8;
+		break;
+
+	default:
+		pr_warning("nuvoton_mono: Unsupported sample rate %d\n",
+			   params_rate(params));
+		return -EINVAL;
+	}
+
+	/* Set CPU and CODEC DAI configuration */
+	ret = snd_soc_dai_set_fmt(codec_dai, fmt);
+	if (ret < 0) {
+		pr_warning("nuvoton_mono: "
+			   "Failed to set CODEC DAI format (%d)\n",
+			   ret);
+		return ret;
+	}
+	ret = snd_soc_dai_set_fmt(cpu_dai, fmt);
+	if (ret < 0) {
+		pr_warning("nuvoton_mono: "
+			   "Failed to set CPU DAI format (%d)\n",
+			   ret);
+		return ret;
+	}
+
+	/* Configure CODEC clocks */
+	pr_debug("nuvoton_mono: "
+		 "pll_in = %ld, pll_out = %u, bclk = %x, mclk = %x\n",
+		 12000000l, pll_out, bclk, mclk_div);
+
+	ret = snd_soc_dai_set_clkdiv(codec_dai, WM8510_BCLKDIV, bclk);
+	if (ret < 0) {
+		pr_warning
+		    ("nuvoton_mono: Failed to set CODEC DAI BCLKDIV (%d)\n",
+		     ret);
+		return ret;
+	}
+
+	ret = snd_soc_dai_set_pll(codec_dai, 0, 0,
+					12000000, pll_out);
+	if (ret < 0) {
+		pr_warning("nuvoton_mono: Failed to set CODEC DAI PLL (%d)\n",
+			   ret);
+		return ret;
+	}
+
+	ret = snd_soc_dai_set_clkdiv(codec_dai, WM8510_MCLKDIV, mclk_div);
+	if (ret < 0) {
+		pr_warning("nuvoton_mono: Failed to set CODEC MCLKDIV (%d)\n",
+			   ret);
+		return ret;
+	}
+
+	return 0;
 }
 
 /* machine stream operations */
